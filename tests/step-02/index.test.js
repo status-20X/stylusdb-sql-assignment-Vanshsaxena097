bbbe6ecae7ad732fb1ae8b3dd69ed2d65b5d1bc2
Vansh450 <vanshsaxena72@gmail.com>
2024-04-13 22:07:44 +0530

    test:2 changed

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -1,28 +1,9 @@
-const fs = require('fs').promises;
+const readCSV = require('../../src/csvReader');
 
-async function readCSV(filePath) {
-    try {
-        const csvData = await fs.readFile(filePath, 'utf-8');
-        const rows = csvData.trim().split('\n');
-        const headers = rows[0].split(',');
-        const data = [];
-
-        for (let i = 1; i < rows.length; i++) {
-            const row = rows[i].split(',');
-            const rowData = {};
-
-            for (let j = 0; j < headers.length; j++) {
-                rowData[headers[j]] = row[j];
-            }
-
-            data.push(rowData);
-        }
-
-        return data;
-    } catch (error) {
-        console.error('Error reading CSV file:', error);
-        throw error;
-    }
-}
-
-module.exports = readCSV;
+test('Read CSV File', async () => {
+    const data = await readCSV('./sample.csv');
+    expect(data.length).toBeGreaterThan(0);
+    expect(data.length).toBe(3);
+    expect(data[0].name).toBe('John');
+    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+});
