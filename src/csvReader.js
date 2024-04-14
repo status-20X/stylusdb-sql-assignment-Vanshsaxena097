7e8bd5d931faaef24cbf99ddd42fd309a66b9a3a
Vansh450 <vanshsaxena72@gmail.com>
2024-04-15 01:47:27 +0530

    #test:2

--- src/csvReader.js
+++ src/csvReader.js
@@ -5,7 +5,7 @@ function readCSV(filePath) {
     const results = [];
 
     return new Promise((resolve, reject) => {
-        fs.createReadStream(filePath)
+        fs.createReadStream('C:\\Users\\Dell\\github-classroom\\status-20X\\stylusdb-sql-assignment-Vanshsaxena097\\tests\\step-02\\sample.csv')
             .pipe(csv())
             .on('data', (data) => results.push(data))
             .on('end', () => {
